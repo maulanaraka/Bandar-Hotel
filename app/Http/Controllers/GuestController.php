d488ef7616d727a900d9880772523feab339838f
AdelioSoebroto <dhioferhan@gmail.com>
2024-05-18 11:07:13 +0700

    Refactor GuestController to improve code organization and add CRUD functionality

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -11,6 +11,9 @@ class GuestController extends Controller
 {
     public function index()
     {
+        $guests = Guest::all();
+        return view('guests.index', compact('guests'));
+
         $guest = Guest::all();
         $data = [
             
@@ -20,6 +23,39 @@ class GuestController extends Controller
         ];
 
         return response()->json($data,200);
+    }
+
+    public function create()
+    {
+        return view('guests.create');
+    }
+
+    public function store(Request $request)
+    {
+        Guest::create($request->all());
+        return redirect()->route('guests.index');
+    }
+
+    public function show(Guest $guest)
+    {
+        return view('guests.show', compact('guest'));
+    }
+
+    public function edit(Guest $guest)
+    {
+        return view('guests.edit', compact('guest'));
+    }
+
+    public function update(Request $request, Guest $guest)
+    {
+        $guest->update($request->all());
+        return redirect()->route('guests.index');
+    }
+
+    public function destroy(Guest $guest)
+    {
+        $guest->delete();
+        return redirect()->route('guests.index');
     }
 
     public function upload(Request $request)
