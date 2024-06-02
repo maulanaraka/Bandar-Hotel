09555fb8c46024fbff28278244681a60d314556e
zulfikarvin <116287620+zulfikarvin@users.noreply.github.com>
2024-06-03 00:36:55 +0700

    revisi link gambar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,11 +73,11 @@
     <h4>Built with love,</h4>
     <h4>Dhio - Raka - Zaki - Laras - Arvin</h4>
     <div class="button-container">
-        <h3><a href="billings"><img src="img/receipt.jpg" alt="Billings"><p>Billings</p></a></h3>
-        <h3><a href="guests"><img src="img/guest.jpg" alt="Guests"><p>Guests</p></a></h3>
-        <h3><a href="reservations"><img src="img/reservations.jpg" alt="Reservations"><p>Reservations</p></a></h3>
-        <h3><a href="reviews"><img src="img/review.jpg" alt="Reviews"><p>Reviews</p></a></h3>
-        <h3><a href="rooms/hotel.php"><img src="img/hotel.jpg" alt="Rooms"><p>Rooms</p></a></h3>
+        <h3><a href="billings"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzM197ysHeAOpYe8TfzIJl_lA43nukvYBcSQ&s" alt="Billings"><p>Billings</p></a></h3>
+        <h3><a href="guests"><img src="https://online.jwu.edu/wp-content/uploads/2023/06/guest20loyalty-min.jpg" alt="Guests"><p>Guests</p></a></h3>
+        <h3><a href="reservations"><img src="https://media.licdn.com/dms/image/C5612AQGPgWwQxrj1Jg/article-cover_image-shrink_423_752/0/1647506988774?e=1723075200&v=beta&t=6__Y8Pk6EbDpsjpyR23KgleXEwCkh3NYgEgi3FKsTY4" alt="Reservations"><p>Reservations</p></a></h3>
+        <h3><a href="reviews"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfcfXb32JFLU6lD_d9n5ry3nTDHN_pn2LoZg&s" alt="Reviews"><p>Reviews</p></a></h3>
+        <h3><a href="rooms/hotel.php"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD5TlRJrGjUVT_BqlfSJgCVdBUlLjN-cvrKQ&s" alt="Rooms"><p>Rooms</p></a></h3>
     </div>
 </body>
 </html>
